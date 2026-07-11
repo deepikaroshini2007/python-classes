c748df54c0000d878d671a426e3a4790049f0a86
deepikaroshini2007 <deepikaroshini2007_bcs29@mepcoeng.ac.in>
2026-07-11 21:02:22 +0530

    shopping_bill.py

--- shopping_bill.py
+++ shopping_bill.py
@@ -1,53 +1,52 @@
 class Shopping:
-def __init__(self):
-self.items = []
-self.quantity = []
-self.price = []
-self.subtotal = []
-self.total = 0
-self.afterdiscount = 0
-self.gst = 0
-self.finalamount = 0
+    def __init__(self):
+        self.items = []
+        self.quantity = []
+        self.price = []
+        self.subtotal = []
+        self.total = 0
+        self.afterdiscount = 0
+        self.gst = 0
+        self.finalamount = 0
 
-def getinput(self):
-n = int(input(&quot;Enter no of products: &quot;))
-for i in range(n):
-item = input(&quot;Enter item name: &quot;)
-self.items.append(item)
-qty = int(input(&quot;Enter quantity: &quot;))
-self.quantity.append(qty)
-price = int(input(&quot;Enter price: &quot;))
-self.price.append(price)
+    def getinput(self):
+        n = int(input("Enter no of products: "))
+        for i in range(n):
+            item = input("Enter item name: ")
+            self.items.append(item)
+            qty = int(input("Enter quantity: "))
+            self.quantity.append(qty)
+            price = int(input("Enter price: "))
+            self.price.append(price)
 
-def subtotalcalc(self):
-for i in range(len(self.items)):
-sub = self.quantity[i] * self.price[i]
-self.subtotal.append(sub)
-self.total += sub
+    def subtotalcalc(self):
+        for i in range(len(self.items)):
+            sub = self.quantity[i] * self.price[i]
+            self.subtotal.append(sub)
+            self.total += sub
 
-def discount(self):
-if self.total &gt; 3000:
-self.afterdiscount = self.total -(self.total*0.10)
+    def discount(self):
+        if self.total > 3000:
+            self.afterdiscount = self.total -(self.total*0.10)
+        else:
+            self.afterdiscount = self.total
 
-else:
-self.afterdiscount = self.total
+    def gstcalc(self):
+        self.gst = self.afterdiscount * 0.05
+        self.finalamount=self.afterdiscount+self.gst
 
-def gstcalc(self):
-self.gst = self.afterdiscount * 0.05
-self.finalamount=self.afterdiscount+self.gst
+    def display(self):
+        print("\n--- BILL ---")
+        print("Item\tQty\tPrice\tSubtotal")
 
-def display(self):
-print(&quot;\n--- BILL ---&quot;)
-print(&quot;Item\tQty\tPrice\tSubtotal&quot;)
+        for i in range(len(self.items)):
+            print(self.items[i], "\t", self.quantity[i], "\t",
+                  self.price[i], "\t", self.subtotal[i])
 
-for i in range(len(self.items)):
-print(self.items[i], &quot;\t&quot;, self.quantity[i], &quot;\t&quot;,
-self.price[i], &quot;\t&quot;, self.subtotal[i])
-
-print(&quot;Total:&quot;, self.total)
-print(&quot;After Discount:&quot;, self.afterdiscount)
-print(&quot;GST:&quot;, self.gst)
-print(&quot;Final Amount:&quot;, self.finalamount)
+        print("Total:", self.total)
+        print("After Discount:", self.afterdiscount)
+        print("GST:", self.gst)
+        print("Final Amount:", self.finalamount)
 
 obj = Shopping()
 obj.getinput()
