8ff8064d0b3f89d4e1f1311969d08a62afca553e
deepikaroshini2007 <deepikaroshini2007_bcs29@mepcoeng.ac.in>
2026-07-11 21:01:32 +0530

    employee_salary.py

--- employee_salary.py
+++ employee_salary.py
@@ -1,34 +1,34 @@
 class Employee:
-def __init__(self,n):
-self.name= []
-self.empid=[]
-self.basic=[]
-self.gross=[]
+    def __init__(self,n):
+        self.name=  []
+        self.empid=[]
+        self.basic=[]
+        self.gross=[]
+    def input_details(self,n):
+        for i in range(n):
+           name=input("Enter Employee Name:")
+           self.name.append(name)
+           empid=int(input("Enter Employee ID:"))
+           self.empid.append(empid)
+           basic=int(input("Enter Basic salary Details:"))
+           self.basic.append(basic)
+    def calculate_salary(self,n):
+            for i in self.basic:
 
-def input_details(self,n):
-for i in range(n):
-name=input(&quot;Enter Employee Name:&quot;)
-self.name.append(name)
-empid=int(input(&quot;Enter Employee ID:&quot;))
-self.empid.append(empid)
-basic=int(input(&quot;Enter Basic salary Details:&quot;))
-self.basic.append(basic)
-def calculate_salary(self,n):
-for i in self.basic:
+                da=0.10*i
+                hra=0.20*i
+                gross=i+da+hra
+                self.gross.append(gross)
+    def display(self):
+        for i in range(n):
+            print(f"-------EMPLOYEE {i+1}-------")
+            print("employee name:",self.name[i])
+            print("employee id:",self.empid[i])
+            print("Salary:",self.gross[i])
 
-da=0.10*i
-hra=0.20*i
-gross=i+da+hra
-self.gross.append(gross)
-def display(self):
-for i in range(n):
-print(f&quot;-------EMPLOYEE {i+1}-------&quot;)
-print(&quot;employee name:&quot;,self.name[i])
-print(&quot;employee id:&quot;,self.empid[i])
-print(&quot;Salary:&quot;,self.gross[i])
-
-n=int(input(&quot;enter no of employee:&quot;))
+n=int(input("enter no of employee:"))
 emp=Employee(n)
 emp.input_details(n)
 emp.calculate_salary(n)
 emp.display()
+
